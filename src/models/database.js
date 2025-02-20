e3981225457142acecc4b5586d717580269c4cff
Rubens-Lira <rubenslira1156@gmail.com>
2025-02-20 16:45:28 -0300

    Arquivo .ENV.DEV adionado

--- src/models/database.js
+++ src/models/database.js
@@ -1,9 +1,19 @@
-import { Sequelize } from "sequelize"
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize("todolist", "postgres", "root", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false
-});
+// Carrega as variáveis de ambiente do arquivo .env.dev
+dotenv.config({ path: '.env.dev' });
 
-export default sequelize
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,    // Nome do banco de dados
+  process.env.DB_USER,    // Usuário
+  process.env.DB_PASSWORD, // Senha
+  {
+    host: process.env.DB_HOST,    // Host do banco de dados
+    dialect: process.env.DB_DIALECT, // Tipo de banco (postgres)
+    logging: false
+  }
+);
+
+export default sequelize;
